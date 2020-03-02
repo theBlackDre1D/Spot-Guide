c35c39c053fe1436869b63ad7297f1ceed204c03
SeremTiNaMeno <michal.gaboriik.77@gmail.com>
2020-03-02 09:24:43 +0100

    fix - better view holders view res ids

--- app/src/main/java/com/example/spotguide/ui/ViewHolders.kt
+++ app/src/main/java/com/example/spotguide/ui/ViewHolders.kt
@@ -8,26 +8,18 @@ import kotlinx.android.synthetic.main.item_spot_photo.view.*
 
 sealed class ViewHolders {
 
-    class SpotPhoto(view: View) : BaseViewHolder<Int>(view) {
-
-        override val viewResId = R.layout.item_spot_photo
+    class SpotPhoto(view: View) : BaseViewHolder<Int>(view, R.layout.item_spot_photo) {
 
         val photo = view.ivSpotPhoto
     }
 
-    class Review(view: View) : BaseViewHolder<Review>(view) {
-        override val viewResId = R.layout.item_review
-
+    class Review(view: View) : BaseViewHolder<Review>(view, R.layout.item_review) {
         val stars = view.vStarRatingBar
         val reviewtext = view.tvReviewBody
     }
 
-    abstract class BaseViewHolder<M: Any>(view: View): RecyclerView.ViewHolder(view), IHasViewResId {
+    abstract class BaseViewHolder<M: Any>(view: View, val viewResId: Int): RecyclerView.ViewHolder(view) {
         val rootView: View
             get() = itemView
-    }
-
-    interface IHasViewResId {
-        val viewResId: Int
     }
 }
